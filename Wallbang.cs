6d8dda57013926d19c2b65e9b68a8acc049ae147
gass-ita <andreanassi231205@gmail.com>
2022-01-09 21:44:44 +0100

    Added Project

--- Wallbang.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Wallbang : MonoBehaviour
-{
-    public bool isWallbengable;
-    public float wallMultiplier;
-}
